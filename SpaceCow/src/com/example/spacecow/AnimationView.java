8f35ef316a7ee6e797b43955394a93002962656f
unknown <MegFennelly18@MFennelly18.MaristSchool.Local>
2015-02-20 13:14:39 -0500

    whatever

---
+++ SpaceCow/src/com/example/spacecow/AnimationView.java
@@ -0,0 +1,13 @@
+package com.example.spacecow;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class AnimationView extends View {
+
+	public AnimationView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		//TODO Auto-generated constructor stub
+	}//end on create
+}//end class Animation View
