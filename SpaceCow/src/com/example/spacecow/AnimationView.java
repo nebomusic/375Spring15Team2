9237e7abbfeb0f70b0ec13b3f67ca94e020afa40
unknown <MegFennelly18@MFennelly18.MaristSchool.Local>
2015-02-23 14:18:39 -0500

    SpaceCow

--- SpaceCow/src/com/example/spacecow/AnimationView.java
+++ SpaceCow/src/com/example/spacecow/AnimationView.java
@@ -1,6 +1,8 @@
 package com.example.spacecow;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,10 +11,23 @@ public class AnimationView extends View {
 	public AnimationView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		//TODO Auto-generated constructor stub
+		
 		//create some actors
 		private Actor cow;
+		
 		//initialize cow
-		cow = new Actor(context, 300, 300, Color.RED, 50)
+		cow = new Actor(context, 300, 300, Color.RED, 50);
 		cow.setCostume(R.drawable.cow);
 	}//end on create
+	
+	//on Draw
+	public void onDraw(Canvas c) {
+		//actors move
+		//actors draw
+		cow.draw(c);
+		
+		//call runnable for animation
+		h.postDelayed(r, RATE);
+		
+	}//end on draw
 }//end class Animation View
