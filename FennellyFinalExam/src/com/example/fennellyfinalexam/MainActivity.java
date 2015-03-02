151f3d7f8a9e777852ad4b883b6deeb8c1b695fb
unknown <MegFennelly18@MFennelly18.MaristSchool.Local>
2015-03-02 14:06:13 -0500

    not done

--- FennellyFinalExam/src/com/example/fennellyfinalexam/MainActivity.java
+++ FennellyFinalExam/src/com/example/fennellyfinalexam/MainActivity.java
@@ -4,31 +4,73 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	
+	//handle click
+	public void handleCick (View v) {
+	
+		
+	}
+			
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	
+		
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+		//Bind to XML
+		buttonDollars = (Button)findViewById(R.id.buttonDollars);
+		buttonYen = (Button)findViewById(R.id.buttonYen);
+		buttonPounds = (Button)findViewById(R.id.buttonPounds);
+		editDollars = (EditText)findViewById(R.id.editDollars);
+		editYen = (EditText)findViewById(R.id.editYen);
+		editPounds = (EditText)findViewById(R.id.editPounds);	
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		
+		 switch(v.getId()) {
+		 case R.id.buttonDollars:
+			 if(clicked) {
+				 String cel= fahrenheitToCelsius(currency);
+				 text2.setText(cel);
+			 }
+		 case R.id.buttonDollars:
+			 if(clicked) {
+				 String fah= celsiusToFahrenheit(currency);
+				 text2.setText(fah);
+			 }
+			 break;
+		 }
+		 
 	}
+
+	private String dollarsToYen(double currency) {
+		double yen = currency * 97.35;
+		return String.valueOf(currency);
+	}
+	private String dollarsToPounds(double currency) {
+		double pounds = currency * .62;
+		return String.valueOf(currency);
+	}
+
+		
+			 
+	
+	
 }
+//end class
